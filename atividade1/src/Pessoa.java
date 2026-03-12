7557d6844c9b1b57c93f58b9420026a80667bce5
Arthur Vieira Lopes <arthur.lopes@feluma.org.br>
2026-03-12 10:54:18 -0300

    Classe pessoa concluída

--- atividade1/src/Pessoa.java
+++ atividade1/src/Pessoa.java
@@ -1,7 +1,48 @@
 public class Pessoa {
 
+        // Atributos
+
         private String nome;
-        
+        private String sobrenome;
+        private int idade;
+        private double altura;
+        private double peso;
+        private double IMC;
+
+        // Métodos
+
+        public double calculaIMC(double peso, double altura){
+
+                this.peso = peso;
+                this.altura = altura;
+                IMC = peso / Math.pow(altura, 2);
+                return IMC;
+
+        }
+
+        public String informaObesidade(){
+
+                if(IMC < 18.5)
+                        return "Abaixo do peso";
+                
+                if(IMC >= 18.5 && IMC <= 24.9)
+                        return "Peso normal";
+
+                if(IMC >= 25 && IMC <= 29.9)
+                        return "Sobrepeso";
+
+                if(IMC >= 30 && IMC <= 34.9)
+                        return "Obesidade grau 1";
+
+                if(IMC >= 35 && IMC <= 39.9)
+                        return "Obesidade grau 2";
+
+                if(IMC >= 40)
+                        return "Obesidade grau 3";
+      
+
+        }
+
 
         
         
