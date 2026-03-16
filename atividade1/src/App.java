026e9f9ff76eaa6814aae4a6ff7bf846b2f1dc6e
Arthur Vieira Lopes <arthur.lopes@feluma.org.br>
2026-03-16 13:06:35 -0300

    Parte 2 finalizada

--- atividade1/src/App.java
+++ atividade1/src/App.java
@@ -1,5 +1,62 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            Scanner sc = new Scanner(System.in);
+
+            int i = 0;
+            String nomeAnterior = null;
+            Pessoa[] pessoas = new Pessoa[10];
+
+            while(i < 10){
+
+                int dia, mes, ano;
+                String nome;
+                double peso, altura;
+
+                System.out.println("Digite seu nome: ");
+                nome = sc.next();
+                
+
+                System.out.println("Digite sua altura: ");
+                altura = sc.nextDouble();
+
+                System.out.println("Digite seu peso: ");
+                peso = sc.nextDouble();
+
+                System.out.println("Digite o dia, mes e ano respectivamente: ");
+                dia = sc.nextInt();
+                mes = sc.nextInt();
+                ano = sc.nextInt();
+                
+                pessoas[i] = new Pessoa(nome, peso, altura);
+                pessoas[i].setDataNascimento(dia, mes, ano);
+
+                if(nomeAnterior != null && nomeAnterior.equals(nome)) {
+                    break;
+                }
+
+                nomeAnterior = nome;
+                
+                i++;
+            }
+
+            for(i = 0; i < 10; i++){
+                
+                System.out.println("\nNome: " + pessoas[i].getNome());
+                System.out.println("\nAltura: " + pessoas[i].getAltura());
+                System.out.println("\nPeso: " + pessoas[i].getPeso());
+                System.out.println("\nData de Nascimento: " + pessoas[i].getDataNascimento().getDia() + " / " + pessoas[i].getDataNascimento().getMes() + " / " + pessoas[i].getDataNascimento().getAno());
+                System.out.println("\nIdade: " + pessoas[i].calcularIdade());
+                System.out.println("\nIMC: " + pessoas[i].calcularIMC());
+                System.out.println("\nNivel Obesidade: " + pessoas[i].informaObesidade());
+
+
+            }
+
+
+
+
+
     }
 }
--- atividade1/src/Pessoa.java
+++ atividade1/src/Pessoa.java
@@ -3,8 +3,56 @@ import java.util.Calendar;
 public class Pessoa {
 
     private Data dataNascimento = new Data();
+    private String nome;
+    private double peso, altura, IMC;
     
+    public Pessoa(){
 
+    }
+
+    public Pessoa(String nome, double peso, double altura){
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getPeso(){
+        return peso;
+    }
+    
+    public double getAltura(){
+        return altura;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome; 
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+    
+    public void setDataNascimento(int dia, int mes, int ano){
+
+        dataNascimento.setDia(dia);
+        dataNascimento.setMes(mes);
+        dataNascimento.setAno(ano);
+
+    }
+
+    public Data getDataNascimento(){
+        return dataNascimento;
+
+    }
+    
     public int calcularIdade(){
 
         Calendar hoje = Calendar.getInstance();
@@ -28,6 +76,43 @@ public class Pessoa {
             return idade;
         }else{
             return idade;
+        }
+
+    }
+
+    public double calcularIMC(){
+
+        IMC = peso/(Math.pow(altura, 2));
+        return peso/(Math.pow(altura, 2));
+
+    }
+
+    public String informaObesidade(){
+
+        if(IMC < 18.5){
+                
+            return "Abaixo do peso";
+
+        }else if(IMC >= 18.5 && IMC <= 24.9){
+
+            return "Peso normal";
+        
+        }else if(IMC >= 25 && IMC <= 29.9){
+
+            return "Sobrepeso";
+        
+        }else if(IMC >= 30 && IMC <= 34.9){
+
+            return "Obesidade grau 1";
+        
+        }else if(IMC >= 35 && IMC <= 39.9){
+
+            return "Obesidade grau 2";
+        
+        }else{
+            
+            return "Obesidade grau 3";
+        
         }
 
     }
